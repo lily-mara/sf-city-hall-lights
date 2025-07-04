13927d07dd8eb9f0fc3b6a2ddf23178b1b0681e6
Lily Mara <lilymara@fastmail.com>
2025-07-04 08:29:25 -0700

    improve error handling and caching logic of generation script

--- generate.py
+++ generate.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 
+from typing import Optional, Union
 from bs4 import BeautifulSoup
 import re
 from datetime import date
 import requests
+import sys
 
 def main():
-    resp = requests.get('https://www.sf.gov/location/san-francisco-city-hall')
+    headers = {}
+
+    etag = read_etag()
+    if etag:
+        headers['If-None-Match'] = etag
+
+    resp = requests.get('https://www.sf.gov/location/san-francisco-city-hall', headers=headers)
+    if resp.status_code == 304:
+        print('Got 304, no new contents')
+        sys.exit(0)
+
+    if resp.status_code != 200:
+        print(f'Got non-OK status code from sf.gov! status={resp.status_code}')
+        print('\n=====\n')
+        print(resp.text)
+        print('\n=====\n')
+        sys.exit(1)
+
     html = resp.text
 
     soup = BeautifulSoup(html, features="html.parser")
@@ -14,6 +33,12 @@ def main():
     with open('city-hall.html', 'w') as f:
         f.write(soup.prettify())
 
+    write_events(soup)
+
+    write_etag(resp)
+
+
+def write_events(soup: BeautifulSoup):
     heading = soup.find('h3', string='Lighting schedule')
     if not heading:
         raise ValueError('Page was missing "lighting schedule" h3 node')
@@ -70,6 +95,27 @@ allday: true
 ---
 """
                     print(contents, file=f)
+
+
+def read_etag() -> Optional[str]:
+    try:
+        with open('.etag') as f:
+            return f.read().strip()
+    except Exception as e:
+        print(f'Failed to load ETag: {e}')
+        return None
+
+
+def write_etag(response: requests.Response):
+    etag = response.headers.get('ETag', None)
+    if not etag:
+        return
+
+    try:
+        with open('.etag', 'w') as f:
+            f.write(etag.strip())
+    except Exception as e:
+        print(f'Failed to save ETag: {e}')
 
 
 if __name__ == "__main__":
